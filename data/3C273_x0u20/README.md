08cf488ef85dc8ef5ddee7a6a62985c1cc063564
Thibault Barnouin <mister.tb@hotmail.fr>
2022-02-28 10:58:55 +0100

    Add 3C273-jet data

---
+++ data/3C273_x0u20/README.md
@@ -0,0 +1,5 @@
+Observation data
+Instrument : FOC
+Proposal ID : 2451
+Target name : 3C273
+Dataset : X0U20
--- src/FOC_reduction.py
+++ src/FOC_reduction.py
@@ -17,11 +17,11 @@ from lib.convex_hull import image_hull
 def main():
     ##### User inputs
     ## Input and output locations
-    globals()['data_folder'] = "../data/NGC1068_x274020/"
-    infiles = ['x274020at.c0f.fits','x274020bt.c0f.fits','x274020ct.c0f.fits',
-            'x274020dt.c0f.fits','x274020et.c0f.fits','x274020ft.c0f.fits',
-            'x274020gt.c0f.fits','x274020ht.c0f.fits','x274020it.c0f.fits']
-    globals()['plots_folder'] = "../plots/NGC1068_x274020/"
+#    globals()['data_folder'] = "../data/NGC1068_x274020/"
+#    infiles = ['x274020at.c0f.fits','x274020bt.c0f.fits','x274020ct.c0f.fits',
+#            'x274020dt.c0f.fits','x274020et.c0f.fits','x274020ft.c0f.fits',
+#            'x274020gt.c0f.fits','x274020ht.c0f.fits','x274020it.c0f.fits']
+#    globals()['plots_folder'] = "../plots/NGC1068_x274020/"
 
 #    globals()['data_folder'] = "../data/NGC1068_x14w010/"
 #    infiles = ['x14w0101t_c0f.fits','x14w0102t_c0f.fits','x14w0103t_c0f.fits',
@@ -81,9 +81,13 @@ def main():
 #    infiles = ['x25d0401t_c0f.fits','x25d0402t_c0f.fits','x25d0403t_c0f.fits']
 #    globals()['plots_folder'] = "../plots/PictorA_x25d040/"
 
+    globals()['data_folder'] = "../data/3C273_x0u20/"
+    infiles = ['x0u20101t_c0f.fits','x0u20102t_c0f.fits','x0u20103t_c0f.fits','x0u20104t_c0f.fits','x0u20105t_c0f.fits','x0u20106t_c0f.fits','x0u20201t_c0f.fits','x0u20202t_c0f.fits','x0u20203t_c0f.fits','x0u20204t_c0f.fits','x0u20205t_c0f.fits','x0u20206t_c0f.fits','x0u20301t_c0f.fits','x0u20302t_c0f.fits','x0u20303t_c0f.fits','x0u20304t_c0f.fits','x0u20305t_c0f.fits','x0u20306t_c0f.fits']
+    globals()['plots_folder'] = "../plots/3C273_x0u20/"
+
     ## Reduction parameters
     # Deconvolution
-    deconvolve = False
+    deconvolve = True
     if deconvolve:
         psf = 'gaussian'  #Can be user-defined as well
         psf_FWHM = 0.10
@@ -106,16 +110,16 @@ def main():
     display_data = False
     # Smoothing
     smoothing_function = 'combine'  #gaussian_after, gaussian or combine
-    smoothing_FWHM = 0.20           #If None, no smoothing is done
+    smoothing_FWHM = 0.10           #If None, no smoothing is done
     smoothing_scale = 'arcsec'       #pixel or arcsec
     # Rotation
     rotate_stokes = True           #rotation to North convention can give erroneous results
     rotate_data = False              #rotation to North convention can give erroneous results
     # Polarization map output
-    figname = 'NGC1068_FOC'         #target/intrument name
-    figtype = '_combine_FWHM020_rot_withaxiserror'    #additionnal informations
-    SNRp_cut = 15.    #P measurments with SNR>3
-    SNRi_cut = 80.   #I measurments with SNR>30, which implies an uncertainty in P of 4.7%.
+    figname = '3C2173_FOC'         #target/intrument name
+    figtype = '_combine_FWHM010_deconvolved'    #additionnal informations
+    SNRp_cut = 5.    #P measurments with SNR>3
+    SNRi_cut = 30.   #I measurments with SNR>30, which implies an uncertainty in P of 4.7%.
     step_vec = 1    #plot all vectors in the array. if step_vec = 2, then every other vector will be plotted
 
     ##### Pipeline start
